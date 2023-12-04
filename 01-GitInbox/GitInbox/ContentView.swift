1d452989022746fe1613f7f81ecb5d9fa36b7b1c
ignasiperez <ignasiperez@gmail.com>
2023-12-04 23:12:45 +0100

    e – New Xcode project Interface: SwiftUI; Language: Swift; Unit Tests on

---
+++ 01-GitInbox/GitInbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitInbox
+//
+//  Created by Ignasi Perez-Valls on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-GitInbox/GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by Ignasi Perez-Valls on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
