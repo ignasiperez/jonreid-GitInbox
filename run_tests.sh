6bb9d07ad9feab8c518f44c21990f301b3bb16b5
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-03-08 21:27:45 +0100

    e - Add explaining variables to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-xcodebuild test -project GitInbox.xcodeproj -scheme GitInbox -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15"
+SCHEME='GitInbox'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
