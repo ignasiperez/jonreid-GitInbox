ca8abdcefe81b22f5f39001f6090a68d925961f6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-03-10 09:56:20 +0100

    d - Add comments to explain why @MainActor annotation has been removed

--- GitInboxTests/GitInboxTests.swift
+++ GitInboxTests/GitInboxTests.swift
@@ -2,6 +2,8 @@ import XCTest
 @testable import GitInbox
 
 // @MainActor
+// If @MainActor annotation is not commented, gives the following warning:
+// "Main actor-isolated class 'GitInboxTests' has different actor isolation from nonisolated superclass 'XCTestCase'; this is an error in Swift 6"
 final class GitInboxTests: XCTestCase {
 
     func test_zero() throws {
